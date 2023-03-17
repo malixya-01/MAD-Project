f94a0093974c247f97a0f0966f817d5bb1721827
maleesha <maleeshahirug@gmail.com>
2023-03-17 16:54:49 +0530

    Nav bar and fragments successful. Next up is nav drawer

--- app/src/main/java/com/example/supportapp/DashboardFragment.kt
+++ app/src/main/java/com/example/supportapp/DashboardFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class NotificationFragment : Fragment() {
+class DashboardFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
 
--- app/src/main/java/com/example/supportapp/MainActivity.kt
+++ app/src/main/java/com/example/supportapp/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.supportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavView: BottomNavigationView
+
+    //Initializing fragments
+    val fragementRequests = RequestsFragment()
+    val fragementDonations = DonationsFragment()
+    val fragementFundrasing = FundrasingFragment()
+    val fragementDashboard = DashboardFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavView = findViewById(R.id.bottom_navigation)
+
+        // display the requests fragment as home page
+        replaceFragment(fragementRequests)
+
+        //Connecting fragments to the nav bar
+        bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.requests -> replaceFragment(fragementRequests)
+                R.id.donorsads -> replaceFragment(fragementDonations)
+                R.id.fundraising -> replaceFragment(fragementFundrasing)
+                R.id.dashboard -> replaceFragment(fragementDashboard)
+                else -> true
+            }
+        }
+
+
+
+
+
     }
+
+    // method to replace the layout with the requests fragment
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        return true
+    }
+
+
 }
