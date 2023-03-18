ea8440833aacd90423e5a28f56e9d2c9b32e3203
maleesha <maleeshahirug@gmail.com>
2023-03-18 21:55:34 +0530

    test commit 2

--- app/src/main/java/com/example/supportapp/MainActivity.kt
+++ app/src/main/java/com/example/supportapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.supportapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +26,18 @@ class MainActivity : AppCompatActivity() {
      */
 
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Creating reequests fragment as HostNav(home page)
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentRequest) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentRequest) as NavHostFragment
         navController=navHostFragment.navController
+
+        //setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+
 
         /*
         bottomNavView = findViewById(R.id.bottom_navigation)
@@ -51,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
          */
 
-        setupActionBarWithNavController(navController)
+
 
     }
 
