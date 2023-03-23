dc3a017c7f71a959451bc880af79b9b91e37b4fb
maleesha <maleeshahirug@gmail.com>
2023-03-23 13:01:59 +0530

    My Fundraisings Card view updated back to the initial card view.

--- app/src/main/java/com/example/supportapp/DataClasses/MyFundraisingsData.kt
+++ app/src/main/java/com/example/supportapp/DataClasses/MyFundraisingsData.kt
@@ -1,3 +1,3 @@
 package com.example.supportapp.DataClasses
 
-data class MyFundraisingsData(val title: String, val location: String, val description: String)
+data class MyFundraisingsData(val title: String, val logo: Int)
