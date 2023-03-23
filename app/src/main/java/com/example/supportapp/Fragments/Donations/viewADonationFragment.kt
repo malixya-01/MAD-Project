bdceae42a6733e6983ad8422de5ef1bee50ccc01
maleesha <maleeshahirug@gmail.com>
2023-03-23 16:19:28 +0530

    add don / edit don done

--- app/src/main/java/com/example/supportapp/Fragments/Donations/viewADonationFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Donations/viewADonationFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.supportapp.R
 
 
@@ -14,6 +16,14 @@ class viewADonationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_a_donation, container, false)
+        val view =  inflater.inflate(R.layout.fragment_view_a_donation, container, false)
+
+        val btnEdit = view.findViewById<Button>(R.id.btnEdit)
+
+        btnEdit.setOnClickListener {
+            findNavController().navigate(R.id.action_viewADonationFragment_to_updateDonationFragment)
+        }
+
+        return view
     }
 }
