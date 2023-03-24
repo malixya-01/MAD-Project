60316a18d7d9a1b7027e08c49a7ce61f744a6ccf
maleesha <maleeshahirug@gmail.com>
2023-03-24 19:06:40 +0530

    view a Request all users done

--- app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/viewARequestAllUsersFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/viewARequestAllUsersFragment.kt
@@ -10,10 +10,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.supportapp.R
 
 class viewARequestAllUsersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
