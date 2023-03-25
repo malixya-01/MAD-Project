a25aee79f7a733cd42bc1e5033a7c68825e61f74
maleesha <maleeshahirug@gmail.com>
2023-03-25 14:47:29 +0530

    fr progress bar working

--- app/src/main/java/com/example/supportapp/Fragments/Fundraisings/viewAFrFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Fundraisings/viewAFrFragment.kt
@@ -6,11 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.navigation.fragment.findNavController
 import com.example.supportapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class viewAFrFragment : Fragment() {
+
+    // variables to function progress bar
+    lateinit var progressBar: ProgressBar
+
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +27,11 @@ class viewAFrFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_view_a_fr, container, false)
+
+        progressBar = view.findViewById(R.id.frProgressBar)
+        progressBar.progress = 165000
+
+
 
         val updateBtn = view.findViewById<FloatingActionButton>(R.id.updateBtn)
         updateBtn.setOnClickListener {
