989349cbc31b29c388b5a28b142ab012f9ffe617
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-04 02:24:43 +0530

    resetPassword btn onclick listner added to ViewProfileFragment

--- app/src/main/java/com/example/supportapp/Fragments/Other/ViewProfileFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Other/ViewProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.supportapp.R
 
 class ViewProfileFragment : Fragment() {
@@ -14,6 +16,15 @@ class ViewProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_view_profile, container, false)
+
+
+        var resetPwd = view.findViewById<Button>(R.id.resetPwd)
+        resetPwd.setOnClickListener {
+            findNavController().navigate(R.id.action_viewProfileFragment_to_resetPasswordFragment)
+        }
+
+
+        return view
     }
 }
