249683ff80d0f49e14c047c915e38c7cdafc6812
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-03 22:29:47 +0530

    modified activity_login.xml

--- app/src/main/java/com/example/supportapp/LoginActivity.kt
+++ app/src/main/java/com/example/supportapp/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.supportapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Hide action bar
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+
+
+
         setContentView(R.layout.activity_login)
     }
 }
