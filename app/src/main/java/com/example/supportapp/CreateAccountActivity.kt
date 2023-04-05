4b42f149de22b0af9273fb0b0a8df05ad6b67492
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-06 02:56:20 +0530

    CreateAccountActivity: upload image to the db success.

--- app/src/main/java/com/example/supportapp/CreateAccountActivity.kt
+++ app/src/main/java/com/example/supportapp/CreateAccountActivity.kt
@@ -1,23 +1,31 @@
 package com.example.supportapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.supportapp.DataClasses.User
 import com.example.supportapp.databinding.ActivityCreateAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class CreateAccountActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateAccountBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
+    private lateinit var storageReference: FirebaseStorage
+    private lateinit var uri: Uri
 
-    private lateinit var image : ImageView
+    //email pattern
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +33,25 @@ class CreateAccountActivity : AppCompatActivity() {
         binding = ActivityCreateAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //initializing storage reference
+        storageReference = FirebaseStorage.getInstance()
+
         //image picker implementation
+        val imageView = binding.ivCreateAccDp
+        val galleryImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                imageView.setImageURI(it)
+                if (it != null) {
+                    uri = it
+                }
+            }
+        )
+
+        //launch gallery
         binding.createAccDpCardView.setOnClickListener {
-            image = binding.ivCreateAccDp
-            uploadImage(image)
+            galleryImage.launch("image/*")
         }
-
-
 
         //Initializing auth and database variables
         auth = FirebaseAuth.getInstance()
@@ -44,7 +64,8 @@ class CreateAccountActivity : AppCompatActivity() {
         } catch (_: NullPointerException) {
         }
 
-        binding.btnCreateAcc.setOnClickListener{
+
+        binding.btnCreateAcc.setOnClickListener {
             val name = binding.etName.text.toString()
             val email = binding.etEmail.text.toString()
             val phone = binding.etPhone.text.toString()
@@ -57,24 +78,24 @@ class CreateAccountActivity : AppCompatActivity() {
             binding.etConfirmPwdLayout.isPasswordVisibilityToggleEnabled = true
 
             //checking if the input fields are empty
-            if(name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty() || confirmPwd.isEmpty()){
-                if(name.isEmpty()){
+            if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty() || confirmPwd.isEmpty()) {
+                if (name.isEmpty()) {
                     binding.etName.error = "Enter your name"
                 }
-                if(email.isEmpty()){
+                if (email.isEmpty()) {
                     binding.etEmail.error = "Enter your email"
                 }
-                if(phone.isEmpty()){
+                if (phone.isEmpty()) {
                     binding.etPhone.error = "Enter your phone"
                 }
-                if(address.isEmpty()){
+                if (address.isEmpty()) {
                     binding.etAddress.error = "Enter your address"
                 }
-                if(password.isEmpty()){
+                if (password.isEmpty()) {
                     binding.etPassword.error = "Enter your password"
                     binding.etPasswordLayout.isPasswordVisibilityToggleEnabled = false
                 }
-                if(confirmPwd.isEmpty()){
+                if (confirmPwd.isEmpty()) {
                     binding.etConfirmPwd.error = "Re-enter your password"
                     binding.etConfirmPwdLayout.isPasswordVisibilityToggleEnabled = false
                 }
@@ -83,69 +104,72 @@ class CreateAccountActivity : AppCompatActivity() {
             }
 
             //validate email pattern
-            else if (!email.matches(emailPattern.toRegex())){
+            else if (!email.matches(emailPattern.toRegex())) {
                 binding.etEmail.error = "Enter a valid email address"
                 binding.signUpProgressbar.visibility = View.GONE
             }
 
             //validate phone number
-            else if (phone.length != 10){
+            else if (phone.length != 10) {
                 binding.etPhone.error = "Enter a valid phone number"
                 binding.signUpProgressbar.visibility = View.GONE
             }
 
             //validate passwords
-            else if (password.length < 7){
+            else if (password.length < 7) {
                 binding.etPassword.error = "Password must be at least 7 characters."
                 binding.etPasswordLayout.isPasswordVisibilityToggleEnabled = false
                 binding.signUpProgressbar.visibility = View.GONE
-            }
-
-            else if (confirmPwd != password){
+            } else if (confirmPwd != password) {
                 binding.etConfirmPwd.error = "Passwords do not match. Please try again."
                 binding.etConfirmPwdLayout.isPasswordVisibilityToggleEnabled = false
                 binding.signUpProgressbar.visibility = View.GONE
-            }
+            } else {
+                //creating an auth for the user
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
 
-            else{
-                //creating a user
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-                    if(it.isSuccessful){
-                        val databaseRef = database.reference.child("users").child(auth.currentUser!!.uid)
+                    if (it.isSuccessful) {
+                        //store user details in the database
+                        val databaseRef =
+                            database.reference.child("users").child(auth.currentUser!!.uid)
                         val user: User = User(name, email, phone, address, auth.currentUser!!.uid)
-
                         databaseRef.setValue(user).addOnCompleteListener {
-                            if (it.isSuccessful){
-                                Toast.makeText(this, "Account created successfully.", Toast.LENGTH_SHORT).show()
+                            if (it.isSuccessful) {
+                                //Upload profile picture to firebase storage
+                                storageReference.getReference("Users").child(auth.currentUser!!.uid)
+                                    .putFile(uri)
+                                    .addOnSuccessListener {
+                                        Toast.makeText(
+                                            this,
+                                            "Account created successfully.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }.addOnFailureListener {
+                                        Toast.makeText(
+                                            this,
+                                            "Failed to upload the image.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+                                //redirect user to login activity
                                 val intent = Intent(this, LoginActivity::class.java)
                                 startActivity(intent)
-                            }
-                            else{
-                                Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(
+                                    this,
+                                    "Something went wrong, try again",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
 
-                    }else{
-                        Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
             }
         }
-    }
-
-    //upload image function
-    private fun uploadImage(image: ImageView) {
-        val intent = Intent()
-        intent.action = Intent.ACTION_GET_CONTENT
-        intent.type = "image/*"
-        startActivityForResult(intent, 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1){
-            image.setImageURI(data?.data)
-        }
-
     }
 }
