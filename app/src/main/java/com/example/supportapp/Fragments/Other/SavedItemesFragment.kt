10ae396e03053dc183db439f3e373c5b69112e35
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-06 23:38:42 +0530

    added NotificationsActivity and SavedItemesActivity activityies

--- app/src/main/java/com/example/supportapp/Fragments/Other/SavedItemesFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Other/SavedItemesFragment.kt
@@ -13,6 +13,9 @@ class SavedItemesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_itemes, container, false)
+        val view =  inflater.inflate(R.layout.fragment_saved_itemes, container, false)
+
+
+        return view
     }
 }
---
+++ app/src/main/java/com/example/supportapp/NotificationsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.supportapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifications)
+    }
+}
