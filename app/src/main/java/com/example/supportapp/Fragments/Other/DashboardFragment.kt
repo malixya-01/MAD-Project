833d26c3d9990e4dc0a4aa77389b6d0c114a67ac
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-07 03:10:28 +0530

    DashboardFragment.kt : retrieve user name done

--- app/src/main/java/com/example/supportapp/Fragments/Other/DashboardFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Other/DashboardFragment.kt
@@ -9,12 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import com.example.supportapp.DataClasses.User
 import com.example.supportapp.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.io.File
@@ -26,7 +28,11 @@ class DashboardFragment : Fragment() {
     private lateinit var databaseReference: DatabaseReference
     private lateinit var storageReference: StorageReference
     private lateinit var uid: String
+    private lateinit var user: User
+    private lateinit var dialog: Dialog
+
     private  lateinit var ivDashboardProfileDp: ImageView
+    private  lateinit var dashboardUserName: TextView
 
 
     override fun onCreateView(
@@ -39,8 +45,12 @@ class DashboardFragment : Fragment() {
         auth = FirebaseAuth.getInstance()
         uid = auth.currentUser?.uid.toString()
 
-        //retrieve user dp
-        getUserProfilePicture()
+        //retrieve data
+        databaseReference = FirebaseDatabase.getInstance().getReference("users")
+        if (uid.isNotEmpty()){
+            getUserData()
+
+        }
 
         //buttons
         var myRequetsBtn = view.findViewById<ConstraintLayout>(R.id.myRequstsBtn)
@@ -48,6 +58,7 @@ class DashboardFragment : Fragment() {
         var myFundrasingsBtn = view.findViewById<ConstraintLayout>(R.id.myFundrasingsBtn)
         ivDashboardProfileDp = view.findViewById(R.id.ivDashboardProfileDp)
 
+        dashboardUserName = view.findViewById(R.id.dashboardUserName)
 
         //rederecting
         myRequetsBtn.setOnClickListener {
@@ -67,6 +78,33 @@ class DashboardFragment : Fragment() {
         }
 
         return view
+
+    }
+
+    private fun getUserData() {
+
+        showProgressBar()
+        databaseReference.child(uid).addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //retrieve values from the db and convert them to user data class
+                user = snapshot.getValue(User::class.java)!!
+
+                //binding data
+                dashboardUserName.setText(user.name)
+
+                //retrieve user profile picture
+                getUserProfilePicture()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                hideProgressBar()
+                Toast.makeText(activity, "Failed to retrieve user data", Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
 
     }
 
@@ -95,14 +133,13 @@ class DashboardFragment : Fragment() {
     }
 
     private fun showProgressBar(){
-        val dialog = Dialog(this@DashboardFragment.requireContext())
+        dialog = Dialog(this@DashboardFragment.requireContext())
         dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.dialog_wait)
         dialog.setCanceledOnTouchOutside(false)
         dialog.show()
     }
     private fun hideProgressBar(){
-        val dialog = Dialog(this@DashboardFragment.requireContext())
         dialog.dismiss()
     }
 
