33d2918d02684cd549aa07c7df8c71f8a03d778e
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-08 00:12:23 +0530

    NewFundraiserFragment: username added to db entry

--- app/src/main/java/com/example/supportapp/Fragments/Fundraisings/NewFundraiserFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Fundraisings/NewFundraiserFragment.kt
@@ -1,27 +1,34 @@
 package com.example.supportapp.Fragments.Fundraisings
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.supportapp.DataClasses.FundraisingData
+import com.example.supportapp.DataClasses.User
 import com.example.supportapp.R
 import com.example.supportapp.databinding.FragmentNewFundraiserBinding
 import com.example.supportapp.models.validations.newFrFormData
 import com.example.supportapp.models.validations.ValidationResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class NewFundraiserFragment : Fragment() {
 
     private lateinit var binding: FragmentNewFundraiserBinding
-    private var isValidationSuccess = false
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
+    private lateinit var databaseRef2: DatabaseReference
+    private lateinit var dialog: Dialog
+    private lateinit var uid: String
+    private lateinit var user: User
+    private var isValidationSuccess = false
+    private var userName: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,19 +40,25 @@ class NewFundraiserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         init(view)
         registerEvents()
-
     }
 
     private fun init(view: View) {
         auth = FirebaseAuth.getInstance()
+        uid = auth.currentUser?.uid.toString()
+
+        //to access the fundraising
         databaseRef = FirebaseDatabase.getInstance().reference
-            .child("fundraising").child(auth.currentUser?.uid.toString())
+            .child("fundraising").child(uid)
 
+        //to access the users
+        databaseRef2 = FirebaseDatabase.getInstance().getReference("users")
 
-
+        //retrieve the username
+        if (uid.isNotEmpty()){
+            getUserData()
+        }
 
     }
 
@@ -55,6 +68,8 @@ class NewFundraiserFragment : Fragment() {
             validateForm()
             if (isValidationSuccess) {
 
+                showProgressBar()
+
                 var title = binding.etNewFrTitle.text.toString()
                 var description = binding.etNewFrDescription.text.toString()
                 var expectedAmt = binding.etNewFrExpectedAmt.text.toString()
@@ -63,15 +78,15 @@ class NewFundraiserFragment : Fragment() {
                 var email = binding.etNewFrEmail.text.toString()
                 var website = binding.etNewFrWebsite.text.toString()
                 var bankDetails = binding.etNewFrBankDetails.text.toString()
-                var username = auth.currentUser?.displayName.toString()
                 var verifiedStatus:Boolean = false
 
-                val fr = FundraisingData(title, description, expectedAmt, collectedAmt, contactNo, email, website, bankDetails, username, verifiedStatus)
+                val fr = FundraisingData(title, description, expectedAmt, collectedAmt, contactNo, email, website, bankDetails, userName, verifiedStatus)
 
                 databaseRef.push().setValue(fr).addOnCompleteListener {
                     if (it.isSuccessful){
-                        Toast.makeText(context, "Your fundraiser added successfully", Toast.LENGTH_SHORT).show()
+                        hideProgressBar()
                         findNavController().navigate(R.id.action_newFundraiserFragment_to_fundrasingFragment)
+                        Toast.makeText(context, "Your fundraiser added successfully", Toast.LENGTH_SHORT).show()
                     } else {
                         Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
                     }
@@ -79,6 +94,21 @@ class NewFundraiserFragment : Fragment() {
                 //Toast.makeText(activity, "Validations passed", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun getUserData() {
+        databaseRef2.child(uid).addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //retrieve values from the db and convert them to user data class
+                user = snapshot.getValue(User::class.java)!!
+                userName = user.name
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(activity, "Failed to retrieve user name", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     fun validateForm() {
@@ -204,6 +234,17 @@ class NewFundraiserFragment : Fragment() {
         }
 
 
+    }
+
+    private fun showProgressBar(){
+        dialog = Dialog( this@NewFundraiserFragment.requireContext())
+        dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+    private fun hideProgressBar(){
+        dialog.dismiss()
     }
 
 }
