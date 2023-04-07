343c0ce93271e3f945dda2108233c592cf4d8906
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-07 20:16:46 +0530

    newFrFormData.kt : updated url pattern regex

--- app/src/main/java/com/example/supportapp/models/newFrFormData.kt
+++ app/src/main/java/com/example/supportapp/models/newFrFormData.kt
@@ -15,7 +15,7 @@ class newFrFormData(
 
     private var frMinimumAmount = 100000
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-    private val urlPattern = "(http(s)?://)?([\\w-]+\\.)+[\\w-]+[.com]+(/[/?%&=]*)?"
+    private val urlPattern = "(http(s)?://)?([\\w-]+\\.)+[\\w-]+\\.+[a-z]+(/[/?%&=]*)?"
     fun validateTitle(): ValidationResult {
         return if(title.isEmpty()){
             ValidationResult.Empty("Title should not be empty")
