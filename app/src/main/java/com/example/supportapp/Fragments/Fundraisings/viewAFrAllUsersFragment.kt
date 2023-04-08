168b33874aed607723139fc035ad81b9e70d4ca5
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-08 13:56:16 +0530

    viewAFrAllUsersFragment.kt : modified toggle saved items

--- app/src/main/java/com/example/supportapp/Fragments/Fundraisings/viewAFrAllUsersFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Fundraisings/viewAFrAllUsersFragment.kt
@@ -11,58 +11,61 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.supportapp.R
+import com.example.supportapp.databinding.FragmentViewAFrAllUsersBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class viewAFrAllUsersFragment : Fragment() {
 
+    private lateinit var binding: FragmentViewAFrAllUsersBinding
+
     // variables to function progress bar
     lateinit var progressBar: ProgressBar
+    private lateinit var fab: FloatingActionButton
+    private var unselectedIcon = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =  inflater.inflate(R.layout.fragment_view_a_fr_all_users, container, false)
+        binding = FragmentViewAFrAllUsersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        //Variables to set up FAB onclick icon change
-        val fab = view.findViewById<FloatingActionButton>(R.id.btnSave)
-        var flag = true // true if first icon is visible, false if second one is visible.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        registerEvents()
+    }
+    private fun init() {
+        progressBar = binding.frProgressBar
+        progressBar.progress = 210000
+    }
+    private fun registerEvents() {
 
-        //varibles to hold toast msgs
-        val itemAdded = resources.getString(R.string.itemAdded)
-        val itemRemoved = resources.getString(R.string.itemRemoved)
-
+        //toggle saved items
+        fab = binding.btnSave
         fab.setOnClickListener {
-            if (flag) {
+            //varibles to hold toast msgs
+            val itemAdded = resources.getString(R.string.itemAdded)
+            val itemRemoved = resources.getString(R.string.itemRemoved)
+
+            // unselectedIcon true if unselected_bookmark icon is visible, false if selected_bookmark icon is visible.
+            if (unselectedIcon) {
                 fab.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.selected_bookmark))
-                flag = false
                 Toast.makeText(requireContext(), itemAdded, Toast.LENGTH_SHORT).show()
+                unselectedIcon = false
 
             } else {
                 fab.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.unselected_bookmark))
-                flag = true
+                unselectedIcon = true
                 Toast.makeText(requireContext(), itemRemoved, Toast.LENGTH_SHORT).show()
-
             }
         }
 
-        progressBar = view.findViewById(R.id.frProgressBar)
-        progressBar.progress = 210000
-
-
-        val btnSup = view.findViewById<Button>(R.id.btnSup)
-        btnSup.setOnClickListener {
+        binding.btnSup.setOnClickListener {
             findNavController().navigate(R.id.action_viewAFrAllUsersFragment_to_sendSupportMsgToAFrFragment)
         }
-
-
-
-
-
-
-
-
-
-        return view
     }
+
+
 }
