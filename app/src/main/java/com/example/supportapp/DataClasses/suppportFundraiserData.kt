583198caaf811e58f7098f13b8edf2f842e2dee4
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-09 19:30:42 +0530

    send support msg to fr db integration complete

---
+++ app/src/main/java/com/example/supportapp/DataClasses/suppportFundraiserData.kt
@@ -0,0 +1,9 @@
+package com.example.supportapp.DataClasses
+
+data class suppportFundraiserData(
+    var msgId: String? = null,
+    var supporterId: String? = null,
+    var email: String? = null,
+    var phoneNumber: String? = null,
+    var message: String? = null
+)
--- app/src/main/java/com/example/supportapp/Fragments/Fundraisings/SendSupportMsgToAFrFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Fundraisings/SendSupportMsgToAFrFragment.kt
@@ -1,18 +1,30 @@
 package com.example.supportapp.Fragments.Fundraisings
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.supportapp.DataClasses.suppportFundraiserData
 import com.example.supportapp.R
 import com.example.supportapp.databinding.FragmentSendSupportMsgToAFrBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class sendSupportMsgToAFrFragment : Fragment() {
 
     private lateinit var binding: FragmentSendSupportMsgToAFrBinding
     val args: sendSupportMsgToAFrFragmentArgs by navArgs()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var dialog: Dialog
+    private var uid: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +36,64 @@ class sendSupportMsgToAFrFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+        registerEvents()
+        //binding.etSupFrMsg.setText(args.currentFrId)
+    }
 
-        binding.etSupFrMsg.setText(args.currentFrId)
+
+
+    private fun init(){
+        auth = FirebaseAuth.getInstance()
+        uid = auth.currentUser?.uid
+        //initialize db ref
+        databaseReference = FirebaseDatabase.getInstance().reference.child("supportFundraiser").child(args.currentFrId)
+    }
+
+    private fun registerEvents(){
+
+        //submit data
+        binding.btnSupFrSubmit.setOnClickListener {
+            showProgressBar()
+            //initialize variables
+            var phone = binding.etSupFrPhone.text.toString()
+            var email = binding.etSupFrEmail.text.toString()
+            var msg = binding.etSupFrMsg.text.toString()
+
+            //Id for new record
+            var id = databaseReference.push().key!!
+
+            //supportFr object
+            var supFr = suppportFundraiserData(id, uid, email, phone, msg)
+
+            //push created object to the db
+            databaseReference.child(id).setValue(supFr).addOnCompleteListener {
+                if (it.isSuccessful){
+                    hideProgressBar()
+                    Toast.makeText(context, "Message sent", Toast.LENGTH_SHORT).show()
+                    clearFormData()
+                } else {
+                    Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun clearFormData() {
+        binding.etSupFrPhone.text = null
+        binding.etSupFrEmail.text = null
+        binding.etSupFrMsg.text = null
+    }
+
+
+    private fun showProgressBar(){
+        dialog = Dialog( this@sendSupportMsgToAFrFragment.requireContext())
+        dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+    private fun hideProgressBar(){
+        dialog.dismiss()
     }
 }
