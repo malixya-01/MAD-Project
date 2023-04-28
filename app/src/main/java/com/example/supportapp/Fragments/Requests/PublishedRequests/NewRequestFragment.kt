7e974d9ee1e09eec141912775cea56a34f68cdb4
Sapun <sapunpramuditha14@gmail.com>
2023-04-28 17:06:24 +0530

    added binding

--- app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/NewRequestFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/NewRequestFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.supportapp.R
+import com.example.supportapp.databinding.FragmentNewRequestBinding
 
 class NewRequestFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewRequestBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_new_request, container, false)
-
-        return view
+        binding = FragmentNewRequestBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //init()
+        registerEvents()
+    }
+
+    /*private fun init() {
+        TODO("Not yet implemented")
+    }*/
+
+    private fun registerEvents() {
+        binding.btnSubmitNewReq.setOnClickListener {
+            Toast.makeText(context, "Yooo", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
