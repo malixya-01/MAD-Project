df1c46162158819b775781980944b7feeea0ca18
Sapun <sapunpramuditha14@gmail.com>
2023-04-28 18:38:00 +0530

    Add new requests complete

--- app/src/main/java/com/example/supportapp/DataClasses/RequestsData.kt
+++ app/src/main/java/com/example/supportapp/DataClasses/RequestsData.kt
@@ -1,3 +1,12 @@
 package com.example.supportapp.DataClasses
 
-data class RequestsData(val title: String,val username: String, val location: String, val description: String)
+data class RequestsData(val title: String? = null,
+                        val username: String? = null,
+                        val location: String? = null,
+                        val description: String? = null,
+                        val phoneNo: String? = null,
+                        val uid: String? = null,
+                        val bankDetails: String? = null,
+                        val date: String? = null,
+                        val reqId: String? = null)
+
--- app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/NewRequestFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/NewRequestFragment.kt
@@ -1,17 +1,42 @@
 package com.example.supportapp.Fragments.Requests.PublishedRequests
 
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.navigation.fragment.findNavController
+import com.example.supportapp.DataClasses.FundraisingData
+import com.example.supportapp.DataClasses.RequestsData
+import com.example.supportapp.DataClasses.User
 import com.example.supportapp.R
 import com.example.supportapp.databinding.FragmentNewRequestBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class NewRequestFragment : Fragment() {
 
     private lateinit var binding: FragmentNewRequestBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseRef: DatabaseReference
+    private lateinit var databaseRefUsers : DatabaseReference
+    private lateinit var dialog: Dialog
+    private lateinit var uid: String
+    private lateinit var user: User
+    private var isValidationSuccess = false
+
+    private var userName: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,18 +48,80 @@ class NewRequestFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //init()
+        init()
         registerEvents()
     }
 
-    /*private fun init() {
-        TODO("Not yet implemented")
-    }*/
+    private fun init() {
+        auth = FirebaseAuth.getInstance()
+        uid = auth.currentUser?.uid.toString()
+
+        //to access the fundraising
+        databaseRef = FirebaseDatabase.getInstance().reference
+            .child("requests")
+
+        //to access the users
+        databaseRefUsers = FirebaseDatabase.getInstance().getReference("users")
+
+        //retrieve the username
+        if (uid.isNotEmpty()){
+            getUserData()
+        }
+    }
 
     private fun registerEvents() {
         binding.btnSubmitNewReq.setOnClickListener {
-            Toast.makeText(context, "Yooo", Toast.LENGTH_SHORT).show()
+            showProgressBar()
+            var title = binding.tvNewReqTitle.text.toString()
+            var city = binding.tvNewReqCity.text.toString()
+            var contactNo = binding.tvNewReqContactNumber.text.toString()
+            var description = binding.tvNewReqDescription.text.toString()
+            var bankDetails = binding.tvNewReqBankDetails.text.toString()
+
+            var date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
+            //Id for new fr
+            var reqId = databaseRef.push().key!!
+            //create a FundraisingData object
+            val reqData = RequestsData(title,userName,city,description,contactNo,uid, bankDetails, date, reqId)
+            databaseRef.child(reqId).setValue(reqData).addOnCompleteListener {
+                if (it.isSuccessful){
+                    hideProgressBar()
+                    findNavController().navigate(R.id.action_newRequestFragment_to_requestsFragment2)
+                    Toast.makeText(context, "Your fundraiser added successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
         }
+    }
+
+    private fun getUserData() {
+        databaseRefUsers.child(uid).addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //retrieve values from the db and convert them to user data class
+                user = snapshot.getValue(User::class.java)!!
+                userName = user.name
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(activity, "Failed to retrieve user name", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun showProgressBar(){
+        dialog = Dialog( this@NewRequestFragment.requireContext())
+        dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+    private fun hideProgressBar(){
+        dialog.dismiss()
     }
 
 }
