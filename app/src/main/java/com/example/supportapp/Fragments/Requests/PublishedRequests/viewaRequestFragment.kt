f42531c2844846732798cf19318ddc51d0771d61
Sapun <sapunpramuditha14@gmail.com>
2023-04-29 10:36:20 +0530

    viewARequestFragment.kt : data retrival done

--- app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/viewaRequestFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Requests/PublishedRequests/viewaRequestFragment.kt
@@ -1,20 +1,33 @@
 package com.example.supportapp.Fragments.Requests.PublishedRequests
 
 import android.app.Dialog
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.supportapp.R
+import com.example.supportapp.databinding.FragmentViewARequestBinding
+import com.example.supportapp.databinding.FragmentViewASingleReqAllUsersBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.File
 
 class viewaRequestFragment : Fragment() {
 
+    private lateinit var binding: FragmentViewARequestBinding
+    private lateinit var storageReference : StorageReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dialog: Dialog
+    private var uid : String? = null
     private val args by navArgs<viewaRequestFragmentArgs>()
 
     override fun onCreateView(
@@ -22,24 +35,47 @@ class viewaRequestFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_view_a_request, container, false)
+        binding = FragmentViewARequestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        var btnEditReq = view.findViewById<FloatingActionButton>(R.id.btnEditReq)
-        btnEditReq.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        registerEvents()
+    }
+
+    private fun init() {
+        auth = FirebaseAuth.getInstance()   //initialize auth
+        uid = auth.currentUser?.uid //initialize current user
+        //retrieve user profile pic
+        storageReference = FirebaseStorage.getInstance().reference
+            .child("Users/$uid")
+        getUserProfilePicture()
+
+        binding.toolbarTitle.title = args.currentReq.title
+        binding.tvName.text = args.currentReq.username
+        binding.tvLoc.text = args.currentReq.location
+        binding.tvDate.text = args.currentReq.date
+        binding.tvDesc.text = args.currentReq.description
+        binding.tvPhone.text = args.currentReq.phoneNo
+        binding.tvBankDetails.text = args.currentReq.bankDetails
+    }
+
+    private fun registerEvents() {
+        binding.btnEditReq.setOnClickListener {
             findNavController().navigate(R.id.action_viewSingleRequestFragment_to_editARequestFragment2)
         }
 
-        val viewDonors = view.findViewById<FloatingActionButton>(R.id.viewDonors)
-        viewDonors.setOnClickListener {
+        binding.viewDonors.setOnClickListener {
             findNavController().navigate(R.id.action_viewSingleRequestFragment_to_viewReqAllDonorsFragment)
         }
 
-        val deleteBtn = view.findViewById<FloatingActionButton>(R.id.btnDlt)
-        deleteBtn.setOnClickListener {
+        binding.btnDlt.setOnClickListener {
             showDeleteConfirmationDialog()
         }
-        return view
     }
+
 
     private fun showDeleteConfirmationDialog() {
         val dialog = Dialog(requireContext())
@@ -62,6 +98,39 @@ class viewaRequestFragment : Fragment() {
         dialog.show()
     }
 
+    private fun getUserProfilePicture() {
+        showProgressBar()
+
+        //create temporary local file to store the retrieved image
+        val localFile = File.createTempFile("tempImage", ".jpg")
+
+        //retrieve image and store it to created temp file
+        storageReference.getFile(localFile).addOnSuccessListener {
+
+            //covert temp file to bitmap
+            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+
+            //bind image
+            binding.ivViewFrDp.setImageBitmap(bitmap)
+
+            hideProgressBar()
+
+        }.addOnFailureListener{
+            hideProgressBar()
+            Toast.makeText(activity, "Failed to retrieve user image", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showProgressBar(){
+        dialog = Dialog(this@viewaRequestFragment.requireContext())
+        dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+    private fun hideProgressBar(){
+        dialog.dismiss()
+    }
 
 
 }
