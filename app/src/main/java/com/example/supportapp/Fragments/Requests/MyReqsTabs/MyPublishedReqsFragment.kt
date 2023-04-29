02ce78307b5e59904950af2c80698fcc6a8076d2
Sapun <sapunpramuditha14@gmail.com>
2023-04-29 09:44:06 +0530

    MyPublishedReqsFragment.kt : data retrival complete

--- app/src/main/java/com/example/supportapp/Fragments/Requests/MyReqsTabs/MyPublishedReqsFragment.kt
+++ app/src/main/java/com/example/supportapp/Fragments/Requests/MyReqsTabs/MyPublishedReqsFragment.kt
@@ -6,19 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.supportapp.Adapters.RequestsAdapter
 import com.example.supportapp.Adapters.myPublishedReqsAdapter
+import com.example.supportapp.DataClasses.RequestsData
 import com.example.supportapp.DataClasses.myPublishedReqsData
+import com.example.supportapp.Fragments.Requests.PublishedRequests.RequestsFragmentDirections
 import com.example.supportapp.R
 import com.example.supportapp.databinding.FragmentMyPublishedReqsBinding
 import com.example.supportapp.databinding.FragmentRequestsBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class myPublishedReqsFragment : Fragment() {
     private lateinit var binding: FragmentMyPublishedReqsBinding
@@ -26,7 +34,7 @@ class myPublishedReqsFragment : Fragment() {
     private lateinit var databaseRef: DatabaseReference
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
-    private var mList = ArrayList<myPublishedReqsData>()
+    private var mList = ArrayList<RequestsData>()
     private lateinit var adapter: myPublishedReqsAdapter
     private lateinit var uid: String
     private lateinit var dialog: Dialog
@@ -45,9 +53,8 @@ class myPublishedReqsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         init()
-        addDataToList()
-        /*retrieveReqs()
-        registerEvents()*/
+        retrieveReqs()
+        registerEvents()
     }
 
     private fun init() {
@@ -64,19 +71,56 @@ class myPublishedReqsFragment : Fragment() {
         recyclerView.adapter = adapter
     }
 
-    /*private fun retrieveReqs() {
-        TODO("Not yet implemented")
+    private fun retrieveReqs() {
+        if (mList.isEmpty()){
+            showProgressBar()
+        }
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for ( frSnapshot in snapshot.children){
+                    val req = frSnapshot.getValue(RequestsData::class.java)!!
+                    if( req != null){
+                        mList.add(req)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+                hideProgressBar()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                hideProgressBar()
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun registerEvents() {
-        TODO("Not yet implemented")
-    }*/
-
-   private fun addDataToList() {
-        mList.add(myPublishedReqsData("My request 1...", "2022/10/30", "10"))
-        mList.add(myPublishedReqsData("My request 2...", "2022/10/30", "10"))
-        mList.add(myPublishedReqsData("My request 3...", "2022/10/30", "10"))
-        mList.add(myPublishedReqsData("My request 4...", "2022/10/30", "10"))
+        adapter.setOnItemClickListener(object : myPublishedReqsAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                val navController = Navigation.findNavController(requireActivity(), R.id.viewPager)
+                navController.navigate(R.id.action_myRequestsFragment_to_viewSingleRequestFragment)
+            }
+        })
     }
+
+    private fun showProgressBar(){
+        dialog = Dialog(this@myPublishedReqsFragment.requireContext())
+        dialog.requestWindowFeature (Window. FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+    private fun hideProgressBar(){
+        dialog.dismiss()
+    }
+
+   /*private fun addDataToList() {
+        mList.add(RequestsData("My request 1...", "2022/10/30", "10"))
+        mList.add(RequestsData("My request 2...", "2022/10/30", "10"))
+        mList.add(RequestsData("My request 3...", "2022/10/30", "10"))
+        mList.add(RequestsData("My request 4...", "2022/10/30", "10"))
+    }*/
 
 }
